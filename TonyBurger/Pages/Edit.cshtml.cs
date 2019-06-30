54d10df078930e97d8790a747f5b800b17fc7ccf
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-30 15:36:34 -0600

    added edit razor page

--- TonyBurger/Pages/Edit.cshtml.cs
+++ TonyBurger/Pages/Edit.cshtml.cs
@@ -4,13 +4,83 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TonyBurger.Data;
+using TonyBurger.Models;
 
 namespace TonyBurger.Pages
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        public Microsoft.AspNetCore.Http.IFormFile file { get; set; }
+        [BindProperty]
+        public byte[] Image { get; set; }
+        public EditModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Burger Burger { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Burger = await _db.Burger.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Burger == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+
+            _db.Attach(Burger).State = EntityState.Modified;
+
+            try
+            {
+                using (var stream = new System.IO.MemoryStream())
+                {
+                    file.CopyTo(stream);
+                    this.Image = stream.ToArray();
+                }
+
+                Burger.Image = this.Image;
+                Burger.ImageContentType = "file";
+
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BurgerExists(Burger.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool BurgerExists(int id)
+        {
+            return _db.Burger.Any(e => e.Id == id);
         }
     }
 }
