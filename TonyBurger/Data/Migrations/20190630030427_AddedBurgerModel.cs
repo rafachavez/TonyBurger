f80a5c0476fd211492e2ee1981b1cb8c1420f791
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-29 21:05:51 -0600

    added migration for new model and updated database

---
+++ TonyBurger/Data/Migrations/20190630030427_AddedBurgerModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TonyBurger.Data.Migrations
+{
+    public partial class AddedBurgerModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Burgers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    ShortDescription = table.Column<string>(nullable: true),
+                    LongDescription = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    ImageThumbnailUrl = table.Column<string>(nullable: true),
+                    IsBurgerOfTheWeek = table.Column<bool>(nullable: false),
+                    Image = table.Column<byte[]>(nullable: true),
+                    ImageContentType = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Burgers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Burgers");
+        }
+    }
+}
