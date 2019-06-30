b84734e0511aa472d399bebe23a7fd492300a19b
rafaeljchavez <42992298+rafaeljchavez@users.noreply.github.com>
2019-06-29 21:58:36 -0600

    adde a migration since i updated the name of the burger table

---
+++ TonyBurger/Data/Migrations/20190630035729_UpdatedBurgerTableName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TonyBurger.Data.Migrations
+{
+    public partial class UpdatedBurgerTableName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Burgers",
+                table: "Burgers");
+
+            migrationBuilder.RenameTable(
+                name: "Burgers",
+                newName: "Burger");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Burger",
+                table: "Burger",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Burger",
+                table: "Burger");
+
+            migrationBuilder.RenameTable(
+                name: "Burger",
+                newName: "Burgers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Burgers",
+                table: "Burgers",
+                column: "Id");
+        }
+    }
+}
